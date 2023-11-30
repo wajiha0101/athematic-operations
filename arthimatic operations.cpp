0b06f07395127c9bfabc88c2d3504c8a0267170d
wajiha0101 <149070831+wajiha0101@users.noreply.github.com>
2023-11-30 22:13:52 +0500

    Add files via upload

---
+++ arthimatic operations.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num1,num2,add,sub,mult;
+	float div;
+	cout<<"enter your number: ";
+	cin>>num1>>num2;
+	add=num1+num2;
+	cout<<"addition: "<<add<<endl;
+	sub=num1-num2;
+	cout<<"subtraction: "<<sub<<endl;
+	mult=num1*num2;
+	cout<<"multiplication: "<<mult<<endl;
+	div=num1/num2;
+	cout<<"division: "<<div<<endl;
+	return 0;
+	
+} 
